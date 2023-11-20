dc57184452e67ee79d0c6d34325b3919a0a97291
Andrey <vproger@vk.com>
2023-11-21 01:11:58 +0400

    add: League\Container and old easy before container, remove: filder test

--- public/index.php
+++ public/index.php
@@ -5,13 +5,12 @@ require_once BASE_PATH.'/vendor/autoload.php';
 
 use Somecode\Framework\Http\Kernel;
 use Somecode\Framework\Http\Request;
-use Somecode\Framework\Routing\Router;
 
 $request = Request::createFromGlobals();
+/** @var League\Container\Container $container */
+$container = require BASE_PATH.'/config/services.php';
+$kernel = $container->get(Kernel::class);
 
-$router = new Router();
-
-$kernel = new Kernel($router);
 $response = $kernel->handle($request);
 
 $response->send();
